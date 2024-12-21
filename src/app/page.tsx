78b1f1a1ad78fb4a7b26e40115ae47717aeb4c27
dn-37 <dn375@mail.ru>
2024-12-21 11:05:18 +0400

    added prisma and created docker compose

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,28 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+
+  console.log(games);
+
   return (
-    <div className="flex justify-center items-center">
-      <Button size="lg" variant="ghost">
-        Hello!
-      </Button>
+    <div className="flex justify-center items-center flex-col">
+      <div className="mb-3">
+        <Button size="lg" variant="ghost">
+          Hello!
+        </Button>
+      </div>
+
+      <div>
+        <ul>
+          {games.map((game) => (
+            <li key={game.id}>
+              {game.id} - {game.name}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
