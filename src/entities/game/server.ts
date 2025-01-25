a2be8a00f770065f691d96024cc590b0001b375a
dn-37 <dn375@mail.ru>
2025-01-25 08:34:28 +0400

    fixed bugs

--- src/entities/game/server.ts
+++ src/entities/game/server.ts
@@ -4,3 +4,4 @@ export { createGame } from "./services/create-game";
 export { getGameById } from "./services/get-game";
 export { startGame } from "./services/start-game";
 export { stepGame } from "./services/step-game";
+export { gameEvents } from "./services/game-events";
--- src/features/games-list/containers/games-list.tsx
+++ src/features/games-list/containers/games-list.tsx
@@ -1,28 +1,8 @@
 import { getIdleGames } from "@/entities/game/server";
-import { GameCard } from "../ui/game-card";
-import { Layout } from "../ui/layout";
-import { CreateButton } from "./create-button";
-import { Button } from "@/shared/ui/button";
-import { routes } from "@/kernel/routes";
-import Link from "next/link";
+import { GamesListClient } from "./games-list-client";
 
 export async function GamesList() {
   const games = await getIdleGames();
 
-  return (
-    <Layout actions={<CreateButton />}>
-      {games.map((game) => (
-        <GameCard
-          key={game.id}
-          login={game.creator.login}
-          rating={game.creator.rating}
-          actions={
-            <Link href={routes.game(game.id)}>
-              <Button>Подключиться</Button>
-            </Link>
-          }
-        ></GameCard>
-      ))}
-    </Layout>
-  );
+  return <GamesListClient games={games} />;
 }
